18b3c8d650ed02ebba245aa120b7e60fbe1b949d
Aleph Melo <alephmelo@icloud.com>
2016-11-28 20:36:32 -0300

    Raw feature is working, needs improv.

---
+++ watchman/__init__.py
@@ -0,0 +1,2 @@
+__AUTHOR__ = 'Aleph Melo'
+__VERSION__ = 0.0.1
--- watchman/main.py
+++ watchman/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from sh import cd, hg
 
 
 def _get_subdirectories(current_dir):
@@ -11,15 +12,23 @@ def check():
     current_working_directory = os.getcwd()
     child_dirs = _get_subdirectories(current_working_directory)
     for child in child_dirs:
-        print("checking folder %s..." % child)
+        if child[0] != '.': 
+            try:
+                cd('%s/%s' % (current_working_directory, child))
+                current_branch = hg('branch')
+                print('%s is on branch: %s' % (child, current_branch), end='')
+                cd('..')
+            except Exception:
+                continue
 
 
 def main():
     arguments = sys.argv    
-    if 'check' in arguments:
+    if 'check' == arguments[1]:
         check()
     else:
         print("type watchman help for, you know, help.")
+
 
 if __name__ == '__main__':
     main()
