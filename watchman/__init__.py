41ecdf65fb8f2462fcc7eff9012c8058ad512694
Aleph Melo <alephmelo@icloud.com>
2016-11-28 20:44:17 -0300

    Add initial docstring.

--- watchman/__init__.py
+++ watchman/__init__.py
@@ -1,2 +1,5 @@
+""" watchman: version control status management made easy."""
+
+Excerpt From: Caleb Hattingh. “20 Python Libraries You Aren’t Using (But Should).” iBooks. 
 __version__ = '0.0.1'
 __author__ = 'Aleph Melo'
