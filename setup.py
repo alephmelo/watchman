e41f3e08540980c4ac2318f5031f9cc6a6435ac6
Aleph Melo <alephmelo@icloud.com>
2016-11-29 23:17:30 -0300

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='watchman',
-      version='0.0.5',
+      version='0.0.6',
       packages=['watchman'],
       entry_points={
           'console_scripts': [
