876d7bc0459da8dc9d32aca71ff25c33c9c8397a
Aleph Melo <alephmelo@icloud.com>
2016-11-29 21:15:44 -0300

    Better argument parsing.

--- watchman/main.py
+++ watchman/main.py
@@ -1,7 +1,11 @@
 from __future__ import print_function
+from argparse import ArgumentParser
 import sys
 import os
-from sh import cd, hg
+from sh import cd, hg, watchman, ls
+
+
+SCM_OPTIONS = ['git', 'hg']
 
 
 def _get_subdirectories(current_dir):
@@ -10,25 +14,34 @@ def _get_subdirectories(current_dir):
             and directory[0] != '.']
 
 
-def check():
-    current_working_directory = os.getcwd()
-    child_dirs = _get_subdirectories(current_working_directory)
-    for child in child_dirs:
-        try:
-            current_branch = hg.branch('-R', './%s' % child)
-            output = '%-25s is on branch: %s' % (child, current_branch)
-            print(output, end='')
-
-        except Exception as e:
-            continue
+def check(scm):
+    if scm in SCM_OPTIONS:
+        current_working_directory = os.getcwd()
+        child_dirs = _get_subdirectories(current_working_directory)
+        for child in child_dirs:
+            try:
+                current_branch = hg.branch('-R', './%s' % child)
+                output = '%-25s is on branch: %s' % (child, current_branch)
+                print(output, end='')
+            except Exception as e:
+                continue
+    else:
+        print("The scm you typed is not supported yet or you just misspelled\nCurrent options are: hg (mercurial) or git.\n")
+        print(watchman('--help'))
 
 
 def main():
-    arguments = sys.argv    
-    if 'check' == arguments[1]:
-        check()
-    else:
-        print("type watchman help for, you know, help.")
+    parser = ArgumentParser()
+    parser.add_argument('check', help='checks branch status of all immediate subdirectories')
+    parser.add_argument('-s',
+                        '--scm',
+                        help='specify source code management application commands\
+                              options: hg (mercurial) or git',
+                        action='store')
+
+    arguments = parser.parse_args()
+    if arguments.check:
+        check(arguments.scm)
 
 
 if __name__ == '__main__':
